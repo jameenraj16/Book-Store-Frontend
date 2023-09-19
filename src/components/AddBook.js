ee97b5f0774d3159a16dc0de9314e0f331cde403
jameenraj16 <jameenraj16@gmail.com>
2023-09-19 23:27:02 +0530

    commit

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -29,7 +29,7 @@ const AddBook = () => {
   };
 
   const sendRequest = async () => {
-    await axios.post("http://localhost:5000/books", {
+    await axios.post("https://dark-gold-snapper-cuff.cyclic.cloud/books", {
       name: String(inputs.name),
       author: String(inputs.author),
       description: String(inputs.description),
